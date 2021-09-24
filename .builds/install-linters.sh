96644fd047114c8c5518eeb7677655e64e1675b5
Rohan Kumar <seirdy@seirdy.one>
2021-09-23 19:21:19 -0700

    Chore(CI): install golangci from master branch
    I'm using some checks from the master branch now, so the config file
    can't be parsed by the @latest version. This results in CI jobs failing.
    
    Tell the CI server to install golangci-lint from @master instead to fix
    this. I don't think i'll hvae to change it back since I frequently
    update the program on my own machine.

--- .builds/install-linters.sh
+++ .builds/install-linters.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-linters='github.com/golangci/golangci-lint/cmd/golangci-lint@latest github.com/praetorian-inc/gokart@latest github.com/mrtazz/checkmake/cmd/checkmake@latest'
+linters='github.com/golangci/golangci-lint/cmd/golangci-lint@master github.com/praetorian-inc/gokart@latest github.com/mrtazz/checkmake/cmd/checkmake@latest'
 
 # first download the linters in parallel, then compile them one by one
 echo "$linters" | tr ' ' '\n' | xargs -P2 -L1 go get -d
